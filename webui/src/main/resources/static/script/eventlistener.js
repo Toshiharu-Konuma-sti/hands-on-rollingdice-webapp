61782a5d898670081713be380492216ca9612067
Toshiharu-Konuma-sti <t-konuma@sios.com>
2025-11-28 14:32:01 +0900

    added to show toast when js excecption occurred

--- webui/src/main/resources/static/script/eventlistener.js
+++ webui/src/main/resources/static/script/eventlistener.js
@@ -1,7 +1,17 @@
 
+function showToast(timestamp) {
+	const x = document.getElementById("toast");
+	x.innerHTML = `JavaScript exception occurred<br>Error triggered! ID: ${timestamp}`;
+	x.className = "show";
+	setTimeout(function(){ 
+		x.className = x.className.replace("show", ""); 
+	}, 3000);
+}
+
 function generateBrowserError(event) {
 	event.preventDefault();
 	const timestamp = Date.now();
+	showToast(timestamp);
 	console.log(`[Faro Demo] Triggered JS error at: ${timestamp}`);
 	throw new Error(`Handson Error: JavaScript error generated at ${timestamp}`);
 }
