6d2f25b632b21c882db0510adc75e2f6193318f4
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-01-24 10:38:37 +0900

    added tasks to use gradlew

--- webapi/RUN.sh
+++ webapi/RUN.sh
@@ -1,15 +1,48 @@
 #!/bin/sh
 
+# {{{ show_usage()
+show_usage()
+{
+	cat << EOS
+Usage: $(basename $0) [options]
+
+Runs this project as a Spring Boot application. Also simplify execution
+of common Gradle tasks.
+
+Options:
+  oas                   Generates the spring doc openapi file and
+                        convert from json to html.
+  javadoc               (Coming soon!!)
+
+EOS
+}
+# }}}
+
+S_TIME=$(date +%s)
 CUR_DIR=$(cd $(dirname $0); pwd)
 . $CUR_DIR/functions.sh
 
-start_banner
-install_jdk
+case "$1" in
+	"oas")
+		start_banner
+		./gradlew generateOpenApiDocs --no-configuration-cache -x cyclonedxBom
+		./gradlew openApiGenerate
+		tree ./build/docs/
+		finish_banner $S_TIME
+		;;
+	"")
+		start_banner
+		install_jdk
 
-echo "Test URL:"
-echo "- http://localhost:8182/api/dice/v1/roll"
-echo "- http://localhost:8182/api/dice/v1/list" 
+		echo "Test URL:"
+		echo "- http://localhost:8182/api/dice/v1/roll"
+		echo "- http://localhost:8182/api/dice/v1/list" 
 
-export SPRING_PROFILES_ACTIVE=dev
-./gradlew clean
-./gradlew bootRun
+		export SPRING_PROFILES_ACTIVE=dev
+		./gradlew clean
+		./gradlew bootRun
+		;;
+	*)
+		show_usage
+		;;
+esac
