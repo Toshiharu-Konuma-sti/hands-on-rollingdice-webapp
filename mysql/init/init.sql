532354ec58ee70c0a15092bd15625304656d9eb7
Toshiharu-Konuma-sti <t-konuma@sios.com>
2026-02-15 03:44:43 +0900

    created mysql user for webapp

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -9,4 +9,8 @@ CREATE TABLE dice(
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) DEFAULT CHARACTER SET=utf8mb4;
 
-GRANT select,insert,update,delete ON hands_on_db.* TO 'myuser'@'%';
+CREATE USER IF NOT EXISTS 'mywebapp'@'%' IDENTIFIED BY 'mypass';
+GRANT select,insert,update,delete ON hands_on_db.* TO 'mywebapp'@'%';
+
+GRANT SELECT ON performance_schema.* TO 'myuser'@'%';
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'myuser'@'%';
